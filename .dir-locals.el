a16dc6dcc364e325f316c903bafc8e478d5062b1
Lectia Landau <lectia@wizard.enterprises>
2024-10-17 19:27:43 +0300

    add test coverage

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,7 @@
 ((clojure-mode
-  . ((cider-clojure-cli-aliases . ":dev:test"))))
+  . ((cider-clojure-cli-aliases . ":dev:test")
+     (eval . (set (make-local-variable 'project-dir)
+                  (file-name-directory
+                   (let ((d (dir-locals-find-file ".")))
+                     (if (stringp d) d (car d))))))
+     (eval . (setq cov-lcov-file-name (concat project-dir "target/coverage/lcov.info"))))))
